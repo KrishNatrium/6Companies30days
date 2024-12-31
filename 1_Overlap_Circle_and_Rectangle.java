86c4e0a65d7f730a1d4e3e6e2c885e0d07aeb772
Krishna <krishnaalegend@gmail.com>
2024-12-31 21:07:57 +0530

    committed

--- 1_Overlap_Circle_and_Rectangle.java
+++ 1_Overlap_Circle_and_Rectangle.java
@@ -1,11 +1,10 @@
 class Solution {
-    private int distance(int x1, int y1, int x2, int y2) {
-        int dx = x2 - x1;
-        int dy = y2 - y1;
-        return (int) Math.sqrt(dx * dx + dy * dy);
-    }
-
     public boolean checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        int nearestX = Math.max(x1, Math.min(x2, xCenter));
+        int nearestY = Math.max(y1, Math.min(y2, yCenter));
 
+        int distX = xCenter - nearestX;
+        int distY = yCenter - nearestY;
+        return distX * distX + distY * distY <= radius * radius;
     }
 }
