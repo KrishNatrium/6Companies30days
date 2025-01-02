9e8f207aeb2c01eb36da5d0d77ae830087001120
Krishna <krishnaalegend@gmail.com>
2025-01-02 21:44:38 +0530

    ques9done

---
+++ 9_Repeated_DNA_Sequences.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        if (s.length() <= 10)
+            return new ArrayList<String>();
+
+        List<String> list = new ArrayList<String>();
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i <= s.length() - 10; i++) {
+            String str = s.substring(i, i + 10);
+            if (map.containsKey(str)) {
+                map.put(str, map.get(str) + 1);
+            } else {
+                map.put(str, 1);
+            }
+        }
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > 1) {
+                list.add(entry.getKey());
+            }
+        }
+
+        return list;
+    }
+}
